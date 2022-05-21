7c8ecbfc74b6459d6575ddae58a836a05c3fdf08
DEV Esteban L <lozadagiraldo.esteban111@gmail.com>
2022-05-21 12:14:10 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/HomeScreen";
+import MachinesScreen from "./components/MachinesScreen";
+import DevicesScreen from "./components/DevicesScreen";
+import GraphScreen from "./components/GraphScreen";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="machines" component={MachinesScreen}></Stack.Screen>
+        <Stack.Screen name="devices" component={DevicesScreen}></Stack.Screen>
+        <Stack.Screen name="graph" component={GraphScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
